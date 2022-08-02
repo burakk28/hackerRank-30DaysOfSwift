d793c285028bba9910b529dc5b88582cf448bb6c
Burak Kara <karaa.burak28@gmail.com>
2022-08-02 13:44:18 +0300

    Add files via upload

---
+++ day01.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var i = 4
+var d = 4.0
+var s = "HackerRank "
+// Declare second integer, double, and String variables.
+var x:Int
+var y:Double
+var z:String
+
+// Read and save an integer, double, and String to your variables.
+x=Int(readLine()!)!
+y=Double(readLine()!)!
+z=readLine()!
+// Print the sum of both integer variables on a new line.
+print(i+x)
+// Print the sum of the double variables on a new line.
+print(d+y)
+// Concatenate and print the String variables on a new line
+// The 's' variable above should be printed first.
+print(s+z)

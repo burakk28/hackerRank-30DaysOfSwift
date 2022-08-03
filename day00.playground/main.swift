82b4f0d6521af88a808364b5163bf8d60e4775ad
Burak Kara <karaa.burak28@gmail.com>
2022-08-03 21:05:16 +0300

    Update main.swift

--- day00.playground/main.swift
+++ day00.playground/main.swift
@@ -1,9 +1,6 @@
 import UIKit
 
-let inputString = readLine()!// get a line of input from stdin and save it to our variable
+let inputString = readLine()!
 
-// Your first line of output goes here
 print("Hello, World.")
-
-// Write the second line of output
 print(inputString)

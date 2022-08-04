eefe2bcbb300108d79a9a3916b1a7c27d1c48db6
Burak Kara <karaa.burak28@gmail.com>
2022-08-04 09:14:38 +0300

    Update Contents.swift

--- day05.playground/Contents.swift
+++ day05.playground/Contents.swift
@@ -3,7 +3,5 @@ import Foundation
 guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
 else { fatalError("Bad input") }
 
-for i in 1 ... 10
-{
-    print("\(n) x \(i) = \(n * i)");
-}
+for i in 1 ... 10{
+    print("\(n) x \(i) = \(n * i)");}
